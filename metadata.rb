f2087c6adff1cbd483ccf0c159da77283faeea1a
Chris Aumann <me@chr4.org>
2014-02-05 14:54:51 +0100

    Add cookbook dependencies
    - postgresql
    - database

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,5 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures pg_repack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
+depends          'database'
+depends          'postgresql'
